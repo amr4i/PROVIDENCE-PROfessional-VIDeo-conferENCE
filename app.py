bf32b87e77a0155d437371daa0feb3a04657d853
Mrinaal Dogra <mrinaald@iitk.ac.in>
2018-11-15 01:40:05 +0530

    1-to-1 P2P video chat using room concept woorks

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, g
 from flask_login import LoginManager
 from flask_socketio import SocketIO
+from flask_socketio import join_room, leave_room
 
 import os
 import login
 import tabledb
 import multistream
+import random as rnd
+import time
 
 app = Flask(__name__)
 
@@ -16,6 +19,12 @@ login_manager.login_view = "home"
 
 # socket io
 socketio = SocketIO(app)
+
+# chat room variables
+list_of_rooms = []
+dict_users = {}
+user_sdp = {}
+user_ice = {}
 
 app.add_url_rule("/", "home", login.home, methods=['GET', 'POST'])
 app.add_url_rule("/login", "login", login.login, methods=['GET', 'POST'])
@@ -40,31 +49,110 @@ def test_video_feed(msg):
     # return multistream.check_connect(msg)
 
 
-# @socketio.on('connect', namespace='/video')
-# def test_video_feed(msg):
-#     print('*'*45)
-#     print(str(msg))
-#     print('*'*45)
-
 
 # @socketio.on('client description', namespace='/video')
-@socketio.on('client description')
-def test_video_feed(json):
-    print('client')
-    # print('*'*45)
-    # print(str(json))
-    # print('*'*45)
-    socketio.emit('peer connect', json, broadcast=True)
+@socketio.on('sdp description')
+def client_description(json):
+    # global list_of_rooms
+    # global dict_users
+
+    # room = rnd.randint(0, 10000)
+    # while room in list_of_rooms:
+    #     room = rnd.randint(0, 10000)
+    #
+    # join_room(room)
+    # list_of_rooms.append(room)
+    # room = int(json['rid'])
+    room = int(json['rid'])
+    username = json['uuid']
+    new_user = json['new_user']
+    # dict_users[room] = [username]
+    # user_sdp[username] = json
+    # print(json['uuid'])
+    # socketio.emit('peer connect', json, broadcast=True)
+    # socketio.emit('peer connect', json, room=room, broadcast=True)
+    # socketio.emit('room id', {'data': room}, room=room)
+    # print(dict_users[room])
+    # print(list_of_rooms)
+    # if room == -1:
+    #     print('Incorrect User: ' + username)
+
+    # for u in dict_users[room]:
+    #     if u in user_sdp.keys():
+    #         socketio.emit('set remote', user_sdp[u], room=room, broadcast=True)
+    json['rid'] = room
+    socketio.emit('set remote', json, room=room, broadcast=True)
+
+
+
+@socketio.on('create room')
+def create_room(json):
+    room = rnd.randint(0, 10000)
+    while room in list_of_rooms:
+        room = rnd.randint(0, 10000)
+
+    join_room(room)
+    list_of_rooms.append(room)
+    username = json['uuid']
+    dict_users[room] = [username]
+    socketio.emit('room id', {'data': room}, room=room)
+
+
+@socketio.on('join')
+def room_join(json):
+    # global list_of_rooms
+    # global dict_users
+
+    room = int(json['rid'])
+
+    if room not in list_of_rooms:
+        print('Why are we here')
+        return
+
+    user = json['uuid']
+    if user in dict_users[room]:
+        print('Why are we here 2')
+        return
+        # dict_users[room].append(user)
+        # join_room(room)
+
+    # for u in dict_users[room]:
+    socketio.emit('create offer', {'new_user': user}, room=room, broadcast=True)
+    dict_users[room].append(user)
+    join_room(room)
+
+    # print(dict_users[room])
+    # for u in dict_users[room]:
+    #     if u in user_sdp.keys():
+    #         socketio.emit('set remote', user_sdp[u], room=room, broadcast=True)
+
+    # time.sleep(0.5)
+    # for u in dict_users[room]:
+    #     if u in user_ice.keys():
+    #         socketio.emit('peer connect', user_ice[u], room=room, broadcast=True)
 
 
 @socketio.on('ice candidate')
 # @socketio.on('ice candidate', namespace='/video')
-def test_video_feed(json):
-    # print('ice')
-    # print('*'*45)
-    # print(str(json))
-    # print('*'*45)
-    socketio.emit('peer connect', json, broadcast=True)
+def ice_candidate(json):
+    room = int(json['rid'])
+    username = json['uuid']
+    # user_ice[username] = json
+
+    if room not in list_of_rooms:
+        return
+
+    if username not in dict_users[room]:
+        return
+
+    # user = json['uuid']
+    # if user not in dict_users[room]:
+    #     dict_users.append(user)
+    #     join_room(room)
+
+    # socketio.emit('peer connect', json, broadcast=True)
+    json['rid'] = room
+    socketio.emit('ice connect', json, room=room, broadcast=True)
 
 
 
--- static/user/js/webrtc.js
+++ static/user/js/webrtc.js
@@ -1,10 +1,12 @@
 var localVideo;
 var localStream;
 var remoteVideo;
-var peerConnection;
+var peerConnection = {};
+var isRemoteSet = {}
 var uuid;
 var username;
 var serverConnection;
+var roomId = -1;
 
 var peerConnectionConfig = {
 	'iceServers': [
@@ -20,46 +22,63 @@ function getUserMediaSuccess(stream) {
 }
 
 
-function gotMessageFromServer(message) {
-	if(!peerConnection) start(false);
+function gotMessageFromServer(signal) {
+	// if(!peerConnection) start(false);
 
-	console.log(message);
-	console.log('************');
+	// console.log(message);
+	// console.log('************');
 	// console.log(message.data);
 	// var signal = JSON.parse(message);
-	var signal = message;
+	// var signal = message;
+	// console.log(signal);
+	console.log("Got Message");
 	console.log(signal);
 
 	// Ignore messages from ourself
 	if(signal.uuid == username) return;
 	// if(signal.uuid == uuid) return;
+	// console.log(signal);
 
-	if(signal.sdp) {
-		peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
-		// Only create answers in response to offers
-		if(signal.sdp.type == 'offer') {
-			peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
-		}
-    }).catch(errorHandler);
-	} else if(signal.ice) {
-		peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
+	if(signal.rid != roomId)
+		return;
+
+	// console.log(signal);
+
+	// if(signal.sdp) {
+	// 	peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
+	// 	// Only create answers in response to offers
+	// 	if(signal.sdp.type == 'offer') {
+	// 		peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
+	// 	}
+ //    }).catch(errorHandler);
+	// } else if(signal.ice) {
+	// 	peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
+	// }
+
+	if(isRemoteSet[signal.uuid] == false)
+		return;
+
+	if(signal.ice) {
+		peerConnection[signal.uuid].addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
 	}
 }
 
 function gotIceCandidate(event) {
+	// console.log('in gotIceCandidate');
 	if(event.candidate != null) {
+		// console.log('in gotIceCandidate if');
 		// serverConnection.send(JSON.stringify({'ice': event.candidate, 'uuid': uuid}));
 		// serverConnection.emit('ice candidate', {'ice': event.candidate, 'uuid': uuid});
-		serverConnection.emit('ice candidate', {'ice': event.candidate, 'uuid': uuid});
+		serverConnection.emit('ice candidate', {'ice': event.candidate, 'uuid': username, 'rid': roomId});
 	}
 }
 
 function createdDescription(description) {
 	console.log('got description');
-	console.log('Hi');
+
 	peerConnection.setLocalDescription(description).then(function() {
 		// serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
-		serverConnection.emit('client description', {'sdp': peerConnection.localDescription, 'uuid': uuid});
+		serverConnection.emit('sdp description', {'sdp': peerConnection.localDescription, 'uuid': username, 'rid': roomId});
 	}).catch(errorHandler);
 }
 
@@ -67,7 +86,11 @@ function gotRemoteStream(event) {
 	console.log('got remote stream');
 	remoteVideo.srcObject = event.streams[0];
 }
+
 function start(isCaller) {
+	// if(!isCaller){
+	// 	console.log('aage kyon nahi bad raha');
+	// }
 	peerConnection = new RTCPeerConnection(peerConnectionConfig);
 	peerConnection.onicecandidate = gotIceCandidate;
 	peerConnection.ontrack = gotRemoteStream;
@@ -75,9 +98,123 @@ function start(isCaller) {
 
 	if(isCaller) {
 		peerConnection.createOffer().then(createdDescription).catch(errorHandler);
+		// serverConnection.emit('create room', {'uuid': username});
 	}
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
+function addIce(signal) {
+	// Ignore messages from ourself
+	if(signal.uuid == username) return;
+
+	if(signal.rid != roomId) return;
+
+	console.log("Add ICE");
+	console.log(signal);
+
+	if(isRemoteSet[signal.uuid] == false)
+		return;
+
+	if(signal.ice) {
+		console.log('Adding Candidate');
+		peerConnection[signal.uuid].addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
+	}
+}
+
+
+
+function setRemoteDesc(signal){
+	if(signal.new_user != username) return;
+
+	if(signal.rid != roomId)
+		return;
+
+	console.log('In setRemoteDesc');
+	console.log(signal);
+
+	if(peerConnection.hasOwnProperty(signal.uuid) == false){
+		peerConnection[signal.uuid] = new RTCPeerConnection(peerConnectionConfig);
+		peerConnection[signal.uuid].onicecandidate = gotIceCandidate;
+		peerConnection[signal.uuid].ontrack = gotRemoteStream;
+		peerConnection[signal.uuid].addStream(localStream);
+	}
+
+	peerConnection[signal.uuid].setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
+		// Only create answers in response to offers
+		isRemoteSet[signal.uuid] = true;
+		if(signal.sdp.type == 'offer') {
+			peerConnection[signal.uuid].createAnswer().then(function(description){
+				console.log('In creating Answer');
+
+				peerConnection[signal.uuid].setLocalDescription(description).then(function() {
+					// serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
+					serverConnection.emit('sdp description', {'sdp': peerConnection[signal.uuid].localDescription,
+																'uuid': username,
+																'rid': roomId,
+																'new_user': signal.uuid
+															});
+				}).catch(errorHandler);
+			}).catch(errorHandler);
+		}
+	}).catch(errorHandler);
+}
+
+
+function createRoom(event){
+	console.log("Create Room");
+	serverConnection.emit('create room', {'uuid': username});
+	// start(true);
+}
+
+
+function joinRoom(event){
+	var x = document.getElementById('joinRoomId');
+	roomId = x.value;
+	console.log("Join Room " + roomId);
+	var p = document.getElementById('roomId');
+	p.innerHTML = roomId;
+
+	serverConnection.emit('join', {'uuid': username, 'rid': roomId});
+	// start(false);
+}
+
+function gotRoomId(message){
+	var p = document.getElementById('roomId');
+	console.log("Got Room Id ");
+	console.log(message);
+	p.innerHTML = message.data;
+	roomId = message.data;
+	// start(true);
+
+	// console.log(message);
+}
+
+
+function createNewOffer(signal) {
+	if (username == signal.new_user)
+		return
+
+	console.log('in createNewOffer');
+	console.log(signal);
+
+	peerConnection[signal.new_user] = new RTCPeerConnection(peerConnectionConfig);
+	peerConnection[signal.new_user].onicecandidate = gotIceCandidate;
+	peerConnection[signal.new_user].ontrack = gotRemoteStream;
+	peerConnection[signal.new_user].addStream(localStream);
+
+	peerConnection[signal.new_user].createOffer().then(function(description) {
+		peerConnection[signal.new_user].setLocalDescription(description).then(function() {
+			// serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
+			serverConnection.emit('sdp description', {'sdp': peerConnection[signal.new_user].localDescription,
+														'uuid': username,
+														'rid': roomId,
+														'new_user': signal.new_user});
+		}).catch(errorHandler);
+	}).catch(errorHandler);
+}
 
 
 function errorHandler(error) {
